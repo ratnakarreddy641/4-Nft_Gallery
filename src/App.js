ed83c0d0e00c9c4803ad127c5a1fdcb632f56dd0
ratnakarreddy641 <ratnu641@gmail.com>
2022-05-25 00:46:31 +0530

    Added Inputs and Buttons

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <input class="ip" placeholder='Wallet Address'   ></input>
-      <input class="ip" placeholder='Contract Address' ></input>
-      <input  type="checkbox" id="Contract_Check"></input>
-      <label for="Contract_Check" >Keep Contract</label>
+      <div class="Inputs"> 
+        <input  placeholder='Wallet Address'   ></input>
+        <input  placeholder='Contract Address' ></input>
+      </div>   
+      <label ><input  type="checkbox" id="GetCollection"></input>Get Collection</label>
       <button>Go</button>
     </div>
   );
